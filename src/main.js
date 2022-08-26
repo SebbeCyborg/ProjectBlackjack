f864379ee875f6bdb853208ac14557dd866aeb39
Sebastian Eriksson <sebastian.eriksson@sectoralarm.se>
2022-08-26 09:05:47 +0200

    website up

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-console.log("Hello world, lets play blackjack")
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World! time for blackjack');
+}).listen(8080);
