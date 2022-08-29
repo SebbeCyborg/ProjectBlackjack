d95a3cde83b0b8f2e267376da0b7fbd926acbb3f
Sebastian Eriksson <sebastian.eriksson@sectoralarm.se>
2022-08-29 09:08:23 +0200

    Player can get 2 cards, we can calculate score

--- src/index.js
+++ src/index.js
@@ -1,135 +1,24 @@
 const suits = ["hearts","diamonds","spades","clubs"];
 const values = ["ace",2,3,4,5,6,7,8,9,10,"kings","queen","jack"];
+
+
+//This class contains the value/suit of a card, and a unique ID.
 class Card {
     constructor(value,suit,id){
         this.value = value;
         this.suit = suit;
         this.id = id;
+        
     }
+    //debugger function
     printCard(){
         console.log("Card ID:"+ this.id);
         console.log("Card Value:"+this.value);
         console.log("Card Suit:"+(this.suit));
         console.log("___________________________\n");
     }
-
-}
-class Hand {
-    //a hand is just two cards
-    constructor(){
-        this.length = 0;
-        this.listOfCards = new Array();
-    }
-    firstCard(){
-        return listOfCards[0];
-    }
-    lastCard(){
-        return listOfCards[length];
-    }
-    length(){
-        return this.length;
-    }
-    addCard(card){
-        this.length++;
-        this.listOfCards.push(card);
-    }
-
 }
 
-/*  */
-function createDeckOfCards(){
-
-const deckSize = 51;
-var cards=[];
-var i = 0
-
-for(j = 0; j <= 3;j++){
-    var suit = suits[j];
-    //for every suit in the deck
-
-    for(k = 0; k <= 12; k++){
-        
-        var value = values[k];
-       
-        //for every value per suit
-        //create a card for every value per suit
-        cards.push( new Card(value,suit,i));
-        cards[i].printCard();
-        i++;
-             
-    }
-}
-
-return cards;
-}
-
-
-
-class Player {
-    constructor(hand,wallet){
-        this.wallet = wallet;
-        this.hand = new Array();
-    }
-
-    printHand(){
-        
-        console.log(this.hand.firstCard());
-        console.log(this.hand.secondCard());
-    }
-   
-}
-
-class gameController{
-    constructor(playerList){
-        this.playerList = [];
-        this.gameOn = true;
-    }
-        
-    isOver21(i){
-        return (i===21);
-    }
-
-    checkHands(){
-        for(i = 0 ; i < this.playerList.length(); i++){
-            isOver21(playerList[i].Player.hand);
-        }
-    }
-}
-
-//pick 2 random cards from deck
-function hit(deck){
-var rndmCard = deck[Math.floor(Math.random() * cards.length)];
-var firstCard = deck.pop(rndmCard);
-var rndmCard2 = deck[Math.floor(Math.random() * cards.length)];
-var secondCard = deck.pop(rndCard2);
-twoCards = new Array();
-
-twoCards.push(rndmCard);
-twoCards.push(rndmCard2);
-
-return twoCards;
-}
-
-
-
-class Dealer{
-    constructor(player,deck){
-        this.player = player;
-        this.deck =deck;
-    }
-    
-
-    hit(){
-        //hits the player with
-        this.player.hand.push(deck.pop());
-
-    }
-    
-    stand(){
-        //does not distribute cards, 
-
-    }
-}
 class  Wallet {
     constructor(balance){
 
@@ -145,11 +34,81 @@ class  Wallet {
     }
 
 }
+class Player {
+    constructor(wallet){
+        this.wallet = wallet;
+        this.score = 0;
+        this.hand = new Array();
+     
+    }
+    addToHand(card){
+        this.hand.push(card);
+
+        if(card.value ==="kings" || card.value === "queen" || card.value === "jack"){
+            this.score = this.score + 10;
+        }
+        
+        if(card.value === "ace"){
+            if(this.score <= 10){
+                this.score = this.score +11;
+            }
+            if(this.score >=10){
+                this.score = this.score + 1;
+            }
+        }
+        if(card.value != "kings" && card.value != "queen" && card.value != "jack" && card.value != "ace"){
+            this.score = this.score + card.value;
+
+        }
+        
+        
+    }
+
+
+    getScore(){
+        if(this.score === 21){
+            console.log("YOU WIN!");
+        }
+
+        if(this.score > 21){
+            console.log("You lost!");
+
+        }
+
+        console.log("SCORE : "+this.score);
+    }
+
+   
+}
+
+/*  */
+function createDeckOfCards(){
+
+    const deckSize = 51;
+    var cards=[];
+    var i = 0
+    
+    for(j = 0; j <= 3;j++){
+        var suit = suits[j];
+        for(k = 0; k <= 12; k++){
+            var value = values[k];
+            cards.push( new Card(value,suit,i));
+            i++;
+        }
+    }
+    
+    return cards;
+}
+
+    
+//pick 1 rabdin card from deck and return
+function hit(deck){
+var rndmCard = deck[Math.floor(Math.random() * deck.length)];
+return deck.pop(rndmCard);;
+}
 
 function shuffle(deckOfCards){
-
     let cardsDeck = deckOfCards;
-
     let shuffledDeck = cardsDeck.sort(function () {
       return Math.random() - 0.5;
     });
@@ -158,13 +117,46 @@ function shuffle(deckOfCards){
 
 }
 
+function stand(deckOfCards){
+    let cardsDeck = deckOfCards;
+    //stand logic
+}
+
+function split(deckOfCards){
+    //split logic
+
+}
+
+function checkScore(handOfCards){
+
+    //do stuff
+    //calculate the score of the hand of cards provided as argument
+    //return true if over 21, false if under, true if it is 21.
+    return isOver21;
+}
 
 //INITIALIZATION
 var roundCounter = 0;
 var outOfMoney = false;
-var wallet = new Wallet(100);
+var wallet = new Wallet(1000);
 var deck = createDeckOfCards();
+
+
+
+
 shuffle(deck);
+var cardFirst = hit(deck);
+var cardSecond = hit(deck);
+
+
+var player = new Player(wallet);
+player.addToHand(cardFirst);
+player.addToHand(cardSecond);
+player.getScore();
+
+/* var cardSecond = hit(deck);
+p = new Player(cardFirst,cardSecond,wallet); */
+
 
 
 
