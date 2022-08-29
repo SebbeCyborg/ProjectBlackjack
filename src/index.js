8a1109cb3a6ce9d408bcbde4fabd52f23ea93100
Sebastian Eriksson <sebastian.eriksson@sectoralarm.se>
2022-08-29 12:40:25 +0200

    making gamestat window show/hide

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 const suits = ["hearts", "diamonds", "spades", "clubs"];
 const values = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "kings", "queen", "jack"];
 
-//This class contains the value/suit of a card, and a unique ID.
 class Card {
   constructor(value, suit, id) {
     this.value = value;
     this.suit = suit;
     this.id = id;
   }
-  //debugger function
+
   printCard() {
     console.log("Card ID:" + this.id);
     console.log("Card Value:" + this.value);
     console.log("Card Suit:" + this.suit);
-    console.log("___________________________\n");
+  
   }
 }
 
@@ -23,8 +22,18 @@ class Player {
     this.score = 0;
     this.hand = new Array();
   }
+
   addToHand(card) {
     this.hand.push(card);
+
+    if (
+        card.value != "kings" &&
+        card.value != "queen" &&
+        card.value != "jack" &&
+        card.value != "ace"
+      ) {
+        this.score = this.score + card.value;
+      }
 
     if (
       card.value === "kings" ||
@@ -32,37 +41,45 @@ class Player {
       card.value === "jack"
     ) {
       this.score = this.score + 10;
+      
     }
 
     if (card.value === "ace") {
-      if (this.score <= 10) {
+      
+      if (this.score < 11) {
         this.score = this.score + 11;
+      
       }
-      if (this.score >= 10) {
+      else if (this.score >= 11) {
+  
         this.score = this.score + 1;
+        
       }
     }
-    if (
-      card.value != "kings" &&
-      card.value != "queen" &&
-      card.value != "jack" &&
-      card.value != "ace"
-    ) {
-      this.score = this.score + card.value;
-    }
+
   }
 
   getScore() {
+    this.printHand();
     if (this.score === 21) {
-      console.log("YOU WIN!");
+      console.log("=WINNER!=");
+      
     }
 
     if (this.score > 21) {
       console.log("You lost!");
+     
     }
 
     console.log("SCORE : " + this.score);
   }
+  
+  printHand(){
+    for( const card of this.hand){
+        //card.printCard();
+    }
+  }
+
 }
 
 /*  */
@@ -83,8 +100,8 @@ function createDeckOfCards() {
   return cards;
 }
 
-//pick 1 rabdin card from deck and return
-function hit(deck) {
+
+function getRandomCard(deck) {
   var rndmCard = deck[Math.floor(Math.random() * deck.length)];
   return deck.pop(rndmCard);
 }
@@ -98,39 +115,44 @@ function shuffle(deckOfCards) {
   return shuffledDeck;
 }
 
-function stand(deckOfCards) {
-  let cardsDeck = deckOfCards;
-  //stand logic
-}
+//DOM VARIABLES
+document.getElementById('buttonStart').addEventListener("click", startGame);
 
-function split(deckOfCards) {
-  //split logic
-}
 
-//INITIALIZATION
+function startGame(){
+    console.log("Start game!");
+    
+}
+//INITIALIZE GAME VARIABLES
 var roundCounter = 0;
 var outOfMoney = false;
 var wallet = 1000;
-var deck = createDeckOfCards();
+var wallet2 = 99999999;
 
+var deck = createDeckOfCards();
 shuffle(deck);
-var cardFirst = hit(deck);
-var cardSecond = hit(deck);
 
 var player = new Player(wallet);
-player.addToHand(cardFirst);
-player.addToHand(cardSecond);
-player.getScore();
+var dealer = new Player(wallet2);
 
-//TODO LIST
+player.addToHand(getRandomCard(deck));
+player.addToHand(getRandomCard(deck));
 
-//give player multiple choices - HIT / STAND / SPLIT
-//catch errors if player choice is invalid/not according to rules
 
-//player chose HIT
+const gameStats = document.getElementById('gameStats');
 
-//player chose STAND
+if(gameStats === null){
+    console.log("NULL TRY AGAIN");
+}
 
-//player chost SPLIT
 
-//player chose to exit game
+
+
+
+
+//making a switch case for when player presses the buttons
+// - hit
+// - split 
+// - stand
+// - submit
+
